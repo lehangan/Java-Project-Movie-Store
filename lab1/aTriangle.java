f5657d08cdf4c0cf6de7da5433fa0aecdec35f83
lehangan <lehangan30@gmail.com>
2023-04-14 10:56:24 +0700

    Exercises

---
+++ lab1/aTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class aTriangle {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int n = keyboard.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for( int k = 1; k<=n-i; k++) 
+                System.out.print(" ");
+            for (int j = 1; j <= 2*i-1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
