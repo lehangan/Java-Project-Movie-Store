963287a09854fd5656c5a7917f0f8c76ebcffddf
lehangan <lehangan30@gmail.com>
2023-05-12 10:55:50 +0700

    lab3

---
+++ lab3/AimsProjectLab3/storelab3/Cart.java
@@ -0,0 +1,51 @@
+package lab3.AimsProjectLab3.storelab3;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private int qtyOrdered = 0;
+    private float totalCost = 0f;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+
+    public int getQtyOrderd() {
+        return this.qtyOrdered;
+    }
+
+    public boolean isFulled() {
+        if (qtyOrdered == MAX_NUMBERS_ORDERED)
+            return true;
+        else
+            return false;
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (isFulled())
+            System.out.println("The Cart is almost full");
+        itemsOrdered[qtyOrdered] = disc;
+        qtyOrdered++;
+        System.out.println("The disc has been added");
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        int index = -1;
+        for (int i = 0; i < 20; i++)
+            if (itemsOrdered[i] == disc)
+                index = i;
+        if (index == -1) {
+            System.out.println("Can't find the disc");
+        } else {
+            qtyOrdered--;
+            for (int i = index; i < qtyOrdered; i++) {
+                itemsOrdered[i] = itemsOrdered[i + 1];
+            }
+        }
+        System.out.println("The disc has been removed");
+    }
+
+    public float getTotalCost() {
+        for (int i = 0; i < qtyOrdered; i++) {
+
+                totalCost += itemsOrdered[i].getCost();
+        }
+        return totalCost;
+    }
+}
