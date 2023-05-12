eba66a244a0c1b9ef3eb615751d6b01b429c5236
lehangan <lehangan30@gmail.com>
2023-05-12 11:14:44 +0700

    method/overloading

--- lab3/AimsProjectLab3/storelab3/Aims.java
+++ lab3/AimsProjectLab3/storelab3/Aims.java
@@ -12,9 +12,15 @@ public class Aims {
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin" , "Animation", 18.995f);
         anOrder.addDigitalVideoDisc(dvd3);
-
+        
         anOrder.removeDigitalVideoDisc(dvd1);
+        DigitalVideoDisc[] dvdList = new DigitalVideoDisc[4];
+        dvdList[0] = new DigitalVideoDisc("dvd1","type1",10f);
+        dvdList[1] = new DigitalVideoDisc("dvd2" ,"type2" , 10f);
+        dvdList[2] = new DigitalVideoDisc("dvd3", "type3" , 10f);
+        dvdList[3] = new DigitalVideoDisc("dvd4", "type4" , 10f);
 
+        anOrder.addDigitalVideoDisc(dvdList);
         System.out.println("Total Cost is:");
         System.out.println(anOrder.getTotalCost());
 
