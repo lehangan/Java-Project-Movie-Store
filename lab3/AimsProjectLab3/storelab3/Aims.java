0694bdf7a43c1802f4346a63382598cf454fd9c0
lehangan <lehangan30@gmail.com>
2023-05-14 15:39:37 +0700

    main

---
+++ lab3/AimsProjectLab3/storelab3/Aims.java
@@ -0,0 +1,22 @@
+package lab3.aimsprojectlab3.storelab3;
+
+public class Aims {
+    public static void main(String[] args){
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King" , "Animation" , "Roger Allers",87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars" , "Science Fiction" , "George Lucas",87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin" , "Animation", 18.995f);
+        anOrder.addDigitalVideoDisc(dvd3);
+        
+        anOrder.removeDigitalVideoDisc(dvd1);
+        System.out.println("Total Cost is:");
+        System.out.println(anOrder.getTotalCost());
+
+       
+    }
+}
