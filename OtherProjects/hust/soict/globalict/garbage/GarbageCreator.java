d3d06faebd725eafc00d7968cd63659ace7d504a
lehangan <lehangan30@gmail.com>
2023-05-15 00:07:12 +0700

    garbage

---
+++ OtherProjects/hust/soict/globalict/garbage/GarbageCreator.java
@@ -0,0 +1,23 @@
+package OtherProjects.hust.soict.globalict.garbage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+    public static void main(String[] args) throws IOException {
+        String filename = "E:/OOP/javaLab/Java/OtherProjects/hust/soict/globalict/garbage/test.txt";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+
+        inputBytes = Files.readAllBytes(Paths.get(filename));
+        startTime = System.currentTimeMillis();
+        String outputString = "";
+        for (byte b : inputBytes) {
+            outputString += (char) b;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
+
+}
